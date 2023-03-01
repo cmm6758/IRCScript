925224037ab13bce1f84fcd74a37ba54e20a9ff8
cmm6758 <118139822+cmm6758@users.noreply.github.com>
2023-03-01 13:43:59 -0600

    Update irchan.py

--- irchan.py
+++ irchan.py
@@ -1,57 +1,72 @@
-from dronekit import connect
-import socket, datetime, time
+#Raytheon 2022-2023 Unmanned Aerial Systems (UAS) Showcase
 
-#Message and channel declared at beginning
-testm = "What's up, buddy? The current time is: "
+# Carlos Mella-Rijo, ID No. 1001736758
+# The University of Texas at Arlington
+# Department of  Electrical Engineering
+# Electrical Engineering Senior Design Team
+
+# This is a script that connects to an IRC Channel and
+# publishes a message when a sensor on our drone is hit, as per
+# the requirements for the competition.
+
+from dronekit import connect # import connection lib from dronekit
+import socket, datetime, time #import socket and time libs 
+
+#Message declared at beginning  along with channel
 msg = "RTXDC_2023_UTA_UGV_Hit_42_"
 channel = "#RTXDrone"
-GPS = "GPSPlaceholder"
-testing = "MOTOR IS RUNNING!!!"
 
-#Commented out for now
+#Establish connection with vehicle
+vehicle = connect('com5', wait_ready=False, baud=57600)
 
-#vehicle = connect('com5', wait_ready=False, baud=57600)
-#lat = vehicle.location.global_relative_frame.lat
-#long = vehicle.location.global_relative_frame.lon
+#Pull GPS Coordinates latitude and longitude from Mavlink stream
+lat = vehicle.location.global_relative_frame.lat
+long = vehicle.location.global_relative_frame.lon
 
-#lat_str = str(lat)
-#lon_str = str(long)
+#Convert GPS coords from int to strings so it can be encoded 
+lat_str = str(lat)
+lon_str = str(long)
 
-#GPS = 'lat_' + lat_str + '_long_' + lon_str
+#Write GPS part of string Message
+GPS = 'lat_' + lat_str + '_long_' + lon_str
 
 
+# Use socket lib to connect to IRC Server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-HOST = 'irc.libera.chat'
-PORT = 6667
-NICK = 'UTA_UGV'
+HOST = 'irc.libera.chat' #Hostname / Server Name
+PORT = 6667 #TCP Port we will connect to server on
+NICK = 'UTA_UGV' #Nickname for UTA UGV
 
+#Connect to specified server on the specified port
 s.connect((HOST, PORT))
 
+#Entering Nickname for UTA UGV bot
 nick_data = ('NICK ' + NICK + '\r\n')
 s.send(nick_data.encode())
 
+#Entering official username for UTA UGV bot
 username_data = ('USER UTAGnd1 UTAGnd2 UTAGnd3 :UTAGnd4 \r\n')
 s.send(username_data.encode())
 
-s.send('JOIN #RTXDrone \r\n'.encode())
+#Joining command following irc protocol 
+s.send('JOIN '+ channel + '\r\n'.encode())
 
-while True:
+while True: #while loop that runs indefinitely
+    #print received messages to screen
     result = s.recv(1024).decode('utf-8')
     print (result)
 
-    hit = vehicle.channels['8']
-    
+    #ping-pong mechanism (so script continuously verifies packets to keep connection going)
     if result[:4] == "PING":
         s.send(("PONG" + result[4:] + "\r\n").encode())
-
+    #disconnect if channel stops sending pings
     if len(result) ==0:
         break
-
+    #if this message is published:
     if "PRIVMSG" in result and "#RTXDrone" in result and ":hello" in result:
+        #Pull current time attribute from datetime class
         now = datetime.datetime.now()
-        time = now.strftime("%I:%M:%S%p")
-        s.send(('PRIVMSG ' + channel + ' :' + testm + time + GPS + '\r\n').encode())
-        
-    #if hit != 0:
-       # s.send(('PRIVMSG ' + channel + ' :' + testing + '\r\n').encode())
-        #time.sleep(3) 
+        #Pull time in 12 hr format and convert to string
+        time = now.strftime("%I:%M:%S_%p_")
+        #Send this message to the channel:
+        s.send(('PRIVMSG ' + channel + ' :' + msg + time + '\r\n').encode())
